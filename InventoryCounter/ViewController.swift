0d42f81c568b6a83e41e4e114667e370ecc26ef4
Timothy Ng <ncy.timothy@gmail.com>
2018-01-20 11:25:13 -0800

    Update UI

--- InventoryCounter/ViewController.swift
+++ InventoryCounter/ViewController.swift
@@ -12,31 +12,31 @@ import Firebase
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var FoodName: UILabel!
-    
-    @IBOutlet weak var FoodCount: UILabel!
-    
-    @IBOutlet weak var Cost: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        FirebaseApp.configure()
-        FoodName.text = CurUser.UserItems?[currentIndex].ItemName
-        FoodCount.text = String(describing: CurUser.UserItems?[currentIndex].ItemCount)
-        CurUser.UserItems?[currentIndex].Cost = Int(arc4random_uniform(10))
-        Cost.text = String(describing: CurUser.UserItems?[currentIndex].Cost)
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    @IBAction func Use(_ sender: UIButton) {
-        CurUser.UserItems?[currentIndex].takeFood()
-        FoodCount.text = String(describing: CurUser.UserItems?[currentIndex].ItemCount)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+//    @IBOutlet weak var FoodName: UILabel!
+//    
+//    @IBOutlet weak var FoodCount: UILabel!
+//    
+//    @IBOutlet weak var Cost: UILabel!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        FirebaseApp.configure()
+//        FoodName.text = currentUser.UserItems?[currentIndex].ItemName
+//        FoodCount.text = String(describing: CurUser.UserItems?[currentIndex].ItemCount)
+//        CurUser.UserItems?[currentIndex].Cost = Int(arc4random_uniform(10))
+//        Cost.text = String(describing: CurUser.UserItems?[currentIndex].Cost)
+//        // Do any additional setup after loading the view, typically from a nib.
+//    }
+//
+//    @IBAction func Use(_ sender: UIButton) {
+//        CurUser.UserItems?[currentIndex].takeFood()
+//        FoodCount.text = String(describing: CurUser.UserItems?[currentIndex].ItemCount)
+//    }
+//    
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
 
 }
